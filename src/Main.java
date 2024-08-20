b260832c1be4cbba3e8761d5200a0dfef2c92dca
herreang000 <herreang000@warren.k12.in.us>
2024-08-20 11:30:12 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        // int result=
+        System.out.println(isLeapYear(10));
+        System.out.println(isLeapYear(9998));
+        System.out.println(isLeapYear(400));
+        System.out.println(isLeapYear(800));
+        System.out.println(isLeapYear(900));
+    }
+
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    //    /400
+                    return year % 400 == 0;
+                } else {
+                    return true;
+                    //     /100
+                }
+            } else {
+                return false;
+                //     /4
+            }
+        }
+        return false;
+    }
 }
+
+
